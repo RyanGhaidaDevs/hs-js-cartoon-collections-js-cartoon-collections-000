b4af667797cdb1d0adff9728b1b4769b3bbd55d6
Ryan Ghaida <ryaboug@gmail.com>
2018-02-24 23:19:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,64 +1,48 @@
 function dwarfRollCall(dwarves) {
 
-  for(var i = 0; i<dwarves.length;i++){
+  var str = ""
 
-   var array1 = dwarves.map(function(x){
+  for (var i = 1; i <= dwarves.length; i++) {
 
-   i++ //why do I have to put this here?
+    str += i + ". " + dwarves[i-1] + " "
 
-   return i + ". " + x
-
- })
-
- var array2 = array1.join(" ")
-
- var array3 = array2 + " "
   }
-  return array3
+  return str
 }
 
+
+
 function summonCaptainPlanet(planeteerCalls){
-
-  var array2 = planeteerCalls.map(function(x){
-    return x.toUpperCase() + "!"
-  })
-
-  return array2
+  return planeteerCalls.map( element => element.toUpperCase() + "!" )
 }
 
 
 function longPlaneteerCalls(words) {
   for(var i = 0; i<words.length;i++){
-
     if(words[i].length>4) {
       return true
     }
-
-    else return false
-
   }
 
+  return false
 }
 
 
-function findTheCheese (food) {
-var cheese = ["cheddar", "gouda", "camembert"]
 
 
-for(var i = 0; i<cheese.length; i++){
+function findTheCheese(food) {
+  var cheeses = ["cheddar", "gouda","camembert"]
+  
+  for(var i = 0; i<cheeses.length; i++){
+    for(var j = 0; j<food.length; j++) {
 
-  for(var j = 0; j<food.length; j++) {
+      if (cheeses[i] == food[j]){
 
-    if (cheese[i] == food[j]){
-
-    return cheese[i]
+      return cheeses[i]
 
 
+      }
+      }
+      return "no cheese!"
     }
-    
-    }
-
-    return "no cheese!"
-  }
-
 }
